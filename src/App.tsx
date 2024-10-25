06b5dc9a318e6ba5205edb0abaae7e711398ff28
Vinícius Lima <vinicius43333@gmail.com>
2024-10-25 10:51:51 -0300

    Basic QR code generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,66 @@
-import { useEffect, useRef } from 'react'
-import QRCodeStyling from 'qr-code-styling'
+import { useState, useEffect, useRef } from 'react'
+import  QRCodeStyling, { FileExtension } from 'qr-code-styling'
 import './App.css'
 
 const qrCode = new QRCodeStyling({
   width: 300,
   height: 300,
-  data: 'http://facebook.com.br',
-  image: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg',
+  data: 'https://facebook.com',
+  backgroundOptions: {
+    color: "#fff",
+  },
   dotsOptions: {
     color: "#4267b2",
-    type: "rounded"
+    type: "square"
   },
   imageOptions: {
+    hideBackgroundDots: false,
     crossOrigin: "anonymous",
-    margin: 20
+    margin: 0
   }
 });
 
 function App() {
+  const [ data, setData ] = useState('https://facebook.com');
+  const [ format, setFormat ] = useState<FileExtension>('png');
   const ref = useRef(null);
 
   useEffect(() => {
     qrCode.append(ref.current!);
   }, []);
 
+  useEffect(() => {
+    qrCode.update({
+      data
+    });
+  }, [data]);
+
+  const updateColor = (color: string) => {
+    qrCode.update({
+      dotsOptions: { color }
+    });
+  };
+
+  const onDonwloadClick = () => {
+    qrCode.download({ name: 'QR code', extension: format });
+  };
+
   return (
     <>
+      <button className='button' onClick={() => updateColor('#d20b2e')}>Vermelho</button>
+      <button className='button' onClick={() => updateColor('#2cba43')}>Verde</button>
+      <button className='button' onClick={() => updateColor('#4267b2')}>Azul</button>
+      <div>
+        <input className='input' style={{ width: '290px' }} type='text' defaultValue='https://facebook.com' onChange={(e) => { e.preventDefault(); setData(e.target.value)}}/>
+      </div>
       <div ref={ref}/>
+      <select className='input' onChange={(e) => setFormat(e.target.value as FileExtension)} style={{ width: '200px' }}>
+        <option value='png'>PNG</option>
+        <option value='jpg'>JPG</option>
+        <option value='webp'>WEBP</option>
+        <option value='svg'>SVG</option>
+      </select>
+      <button className='button button-primary' onClick={onDonwloadClick}>Download</button>
     </>
   )
 }
