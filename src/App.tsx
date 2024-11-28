bb4d78fed7658d2389317c4ee66ef9f005a55fc8
Vinícius Lima <vinicius43333@gmail.com>
2024-11-28 10:09:00 -0300

    Improved the responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -117,209 +117,216 @@ function App() {
   };
 
   return (
-    <div className='container my-10 mx-auto flex space-x-3'>
-      <div className='w-full space-y-3'>
-        <Surface className='w-full space-y-3'>
-          {/* QR code data options */}
-          <p className='font-semibold text-lg text-slate-600'>URL</p>
-          <Input
-            defaultValue='https://facebook.com'
-            onChange={(e) => setOptions({ ...options, data: e.target.value})}
-            className='w-full'
-          />
-        </Surface>
-        <Surface className='w-full space-y-3'>
-          {/* QR code template options */}
-          <p className='font-semibold text-lg text-slate-600'>Templates</p>
-          <div className='flex space-x-3'>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={default_template}
-                onClick={() => changeTemplate(defaultOptions)}
-                isActive={options === defaultOptions}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={love_template}
-                onClick={() => changeTemplate(pinkPurpleTemplate)}
-                isActive={options === pinkPurpleTemplate}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={christimas_template}
-                onClick={() => changeTemplate(christmasTemplate)}
-                isActive={options === christmasTemplate}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={lemonade_template}
-                onClick={() => changeTemplate(lemonadeTemplate)}
-                isActive={options === lemonadeTemplate}
-              />
-            </div>
-          </div>
-        </Surface>
-        <Surface className='w-full space-y-3'>
-          {/* QR code dot options */}
-          <p className='font-semibold text-lg text-slate-600'>Dots</p>
-          <div className='flex space-x-3'>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_square}
-                onClick={() => changeDotType('square')}
-                isActive={options?.dotsOptions?.type === 'square'}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_rounded}
-                onClick={() => changeDotType('rounded')}
-                isActive={options?.dotsOptions?.type === 'rounded'}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_dots}
-                onClick={() => changeDotType('dots')}
-                isActive={options?.dotsOptions?.type === 'dots'}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_classy}
-                onClick={() => changeDotType('classy')}
-                isActive={options?.dotsOptions?.type === 'classy'}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_classy_rounded}
-                onClick={() => changeDotType('classy-rounded')}
-                isActive={options?.dotsOptions?.type === 'classy-rounded'}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={dots_extra_rounded}
-                onClick={() => changeDotType('extra-rounded')}
-                isActive={options?.dotsOptions?.type === 'extra-rounded'}
-              />
-            </div>
-          </div>
-          <ColorPicker onChange={(e) => changeDotColor(e.target.value)} value={options?.dotsOptions?.color}/>
-        </Surface>
-        <div className='flex w-full space-x-3'>
-          <Surface className='w-6/12 space-y-3'>
-            {/* QR code corner square options */}
-            <p className='font-semibold text-lg text-slate-600'>Corner Square</p>
-            <div className='flex space-x-3'>
-              <div className={'w-2/12'}>
-                <OptionButton
-                  image={none}
-                  onClick={() => changeCornerSquareType(undefined)}
-                  isActive={options?.cornersSquareOptions?.type === undefined}
-                />
-              </div>
-              <div className={'w-2/12'}>
-                <OptionButton
-                  image={corner_square}
-                  onClick={() => changeCornerSquareType('square')}
-                  isActive={options?.cornersSquareOptions?.type === 'square'}
-                />
-              </div>
-              <div className={'w-2/12'}>
-                <OptionButton
-                  image={corner_dot}
-                  onClick={() => changeCornerSquareType('dot')}
-                  isActive={options?.cornersSquareOptions?.type === 'dot'}
-                />
-              </div>
-              <div className={'w-2/12'}>
-                <OptionButton
-                  image={corner_rounded}
-                  onClick={() => changeCornerSquareType('extra-rounded')}
-                  isActive={options?.cornersSquareOptions?.type === 'extra-rounded'}
-                />
-              </div>
-            </div>
-            <ColorPicker onChange={(e) => changeCornerSquareColor(e.target.value)} value={options?.cornersSquareOptions?.color}/>
+    <div className='container my-10 mx-auto'>
+      <div className='md:flex mx-3 md:space-x-3 max-md:space-y-3'>
+        <div className='w-full space-y-3'>
+          <Surface className='w-full space-y-3'>
+            {/* QR code data options */}
+            <p className='font-semibold text-lg text-slate-600'>URL</p>
+            <Input
+              defaultValue='https://facebook.com'
+              onChange={(e) => setOptions({ ...options, data: e.target.value})}
+              className='w-full'
+            />
           </Surface>
-          <Surface className='w-6/12 space-y-3'>
-            {/* QR corner dots options */}
-            <p className='font-semibold text-lg text-slate-600'>Corner Dots</p>
-            <div className='flex space-x-3'>
-              <div className={'w-2/12'}>
+          <Surface className='w-full space-y-3'>
+            {/* QR code template options */}
+            <p className='font-semibold text-lg text-slate-600'>Templates</p>
+            <div className='grid max-md:grid-cols-4 md:grid-cols-12 gap-3'>
+              <div className={'w-full'}>
                 <OptionButton
-                  image={none}
-                  onClick={() => changeCornerDotType(undefined)}
-                  isActive={options?.cornersDotOptions?.type === undefined}
+                  image={default_template}
+                  onClick={() => changeTemplate(defaultOptions)}
+                  isActive={options === defaultOptions}
                 />
               </div>
-              <div className={'w-2/12'}>
+              <div className={'w-full'}>
                 <OptionButton
-                  image={square}
-                  onClick={() => changeCornerDotType('square')}
-                  isActive={options?.cornersDotOptions?.type === 'square'}
+                  image={love_template}
+                  onClick={() => changeTemplate(pinkPurpleTemplate)}
+                  isActive={options === pinkPurpleTemplate}
                 />
               </div>
-              <div className={'w-2/12'}>
+              <div className={'w-full'}>
                 <OptionButton
-                  image={dot}
-                  onClick={() => changeCornerDotType('dot')}
-                  isActive={options?.cornersDotOptions?.type === 'dot'}
+                  image={christimas_template}
+                  onClick={() => changeTemplate(christmasTemplate)}
+                  isActive={options === christmasTemplate}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={lemonade_template}
+                  onClick={() => changeTemplate(lemonadeTemplate)}
+                  isActive={options === lemonadeTemplate}
                 />
               </div>
             </div>
-            <ColorPicker onChange={(e) => changeCornerDotColor(e.target.value)} value={options?.cornersDotOptions?.color}/>
+          </Surface>
+          <Surface className='w-full space-y-3'>
+            {/* QR code dot options */}
+            <p className='font-semibold text-lg text-slate-600'>Dots</p>
+            <div className='grid max-md:grid-cols-4 md:grid-cols-12 gap-3'>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_square}
+                  onClick={() => changeDotType('square')}
+                  isActive={options?.dotsOptions?.type === 'square'}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_rounded}
+                  onClick={() => changeDotType('rounded')}
+                  isActive={options?.dotsOptions?.type === 'rounded'}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_dots}
+                  onClick={() => changeDotType('dots')}
+                  isActive={options?.dotsOptions?.type === 'dots'}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_classy}
+                  onClick={() => changeDotType('classy')}
+                  isActive={options?.dotsOptions?.type === 'classy'}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_classy_rounded}
+                  onClick={() => changeDotType('classy-rounded')}
+                  isActive={options?.dotsOptions?.type === 'classy-rounded'}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={dots_extra_rounded}
+                  onClick={() => changeDotType('extra-rounded')}
+                  isActive={options?.dotsOptions?.type === 'extra-rounded'}
+                />
+              </div>
+            </div>
+            <ColorPicker onChange={(e) => changeDotColor(e.target.value)} value={options?.dotsOptions?.color}/>
+          </Surface>
+          <div className='md:flex w-full max-md:space-y-3 md:space-x-3'>
+            <Surface className='md:w-6/12 space-y-3'>
+              {/* QR code corner square options */}
+              <p className='font-semibold text-lg text-slate-600'>Corner Square</p>
+              <div className='grid max-md:grid-cols-4 md:grid-cols-6 gap-3'>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={none}
+                    onClick={() => changeCornerSquareType(undefined)}
+                    isActive={options?.cornersSquareOptions?.type === undefined}
+                  />
+                </div>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={corner_square}
+                    onClick={() => changeCornerSquareType('square')}
+                    isActive={options?.cornersSquareOptions?.type === 'square'}
+                  />
+                </div>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={corner_dot}
+                    onClick={() => changeCornerSquareType('dot')}
+                    isActive={options?.cornersSquareOptions?.type === 'dot'}
+                  />
+                </div>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={corner_rounded}
+                    onClick={() => changeCornerSquareType('extra-rounded')}
+                    isActive={options?.cornersSquareOptions?.type === 'extra-rounded'}
+                  />
+                </div>
+              </div>
+              <ColorPicker onChange={(e) => changeCornerSquareColor(e.target.value)} value={options?.cornersSquareOptions?.color}/>
+            </Surface>
+            <Surface className='md:w-6/12 space-y-3'>
+              {/* QR corner dots options */}
+              <p className='font-semibold text-lg text-slate-600'>Corner Dots</p>
+              <div className='grid max-md:grid-cols-4 md:grid-cols-6 gap-3'>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={none}
+                    onClick={() => changeCornerDotType(undefined)}
+                    isActive={options?.cornersDotOptions?.type === undefined}
+                  />
+                </div>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={square}
+                    onClick={() => changeCornerDotType('square')}
+                    isActive={options?.cornersDotOptions?.type === 'square'}
+                  />
+                </div>
+                <div className={'w-full'}>
+                  <OptionButton
+                    image={dot}
+                    onClick={() => changeCornerDotType('dot')}
+                    isActive={options?.cornersDotOptions?.type === 'dot'}
+                  />
+                </div>
+              </div>
+              <ColorPicker onChange={(e) => changeCornerDotColor(e.target.value)} value={options?.cornersDotOptions?.color}/>
+            </Surface>
+          </div>
+          <Surface className='w-full space-y-3'>
+            {/* QR code logo options */}
+            <p className='font-semibold text-lg text-slate-600'>Logo</p>
+            <div className='grid max-md:grid-cols-4 md:grid-cols-12 gap-3'>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={hearth}
+                  onClick={() => changeLogo(hearth)}
+                  isActive={options?.image === hearth}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={holly_berry}
+                  onClick={() => changeLogo(holly_berry)}
+                  isActive={options?.image === holly_berry}
+                />
+              </div>
+              <div className={'w-full'}>
+                <OptionButton
+                  image={lemon}
+                  onClick={() => changeLogo(lemon)}
+                  isActive={options?.image === lemon}
+                />
+              </div>
+            </div>
           </Surface>
         </div>
-        <Surface className='w-full space-y-3'>
-          {/* QR code logo options */}
-          <p className='font-semibold text-lg text-slate-600'>Logo</p>
-          <div className='flex space-x-3'>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={hearth}
-                onClick={() => changeLogo(hearth)}
-                isActive={options?.image === hearth}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={holly_berry}
-                onClick={() => changeLogo(holly_berry)}
-                isActive={options?.image === holly_berry}
-              />
-            </div>
-            <div className={'w-1/12'}>
-              <OptionButton
-                image={lemon}
-                onClick={() => changeLogo(lemon)}
-                isActive={options?.image === lemon}
-              />
-            </div>
+        <Surface className='space-y-3 h-full'>
+          {/* QR code preview area */}
+          <div className='md:w-96 py-8 flex justify-center'>
+            <div ref={ref}/>
+          </div>
+          {/* Download options */}
+          <div className='md:w-96 flex space-x-3'>
+            <Select onChange={(e: any) => setFormat(e.target.value as FileExtension)}>
+              <option value='png'>PNG</option>
+              <option value='jpg'>JPG</option>
+              <option value='webp'>WEBP</option>
+              <option value='svg'>SVG</option>
+            </Select>
+            <Button onClick={onDonwloadClick}>Download</Button>
           </div>
         </Surface>
       </div>
-      <Surface className='space-y-3 h-full'>
-        {/* QR code preview area */}
-        <div className='w-96 h-96 py-8 flex justify-center'>
-          <div ref={ref}/>
-        </div>
-        {/* Download options */}
-        <div className='w-96 flex space-x-3'>
-          <Select onChange={(e: any) => setFormat(e.target.value as FileExtension)}>
-            <option value='png'>PNG</option>
-            <option value='jpg'>JPG</option>
-            <option value='webp'>WEBP</option>
-            <option value='svg'>SVG</option>
-          </Select>
-          <Button onClick={onDonwloadClick}>Download</Button>
-        </div>
-      </Surface>
+      <div className='w-full text-center my-5'>
+        <p className='font-semibold text-md text-slate-500'>
+          Made with ❤️ by Vinícius Lima
+        </p>
+      </div>
     </div>
   );
 }
